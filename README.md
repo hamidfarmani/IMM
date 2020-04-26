ff46a9170e9872aba1bb5395f73621aae26dda87
Hamid <hamidfarmani1@gmail.com>
2020-04-26 15:59:26 +0430

    - add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+Interactions between MongoDB and MySQL in Grails/Groovy 
+=======================================================
+Main functionalities
+--------------------
+1. Upload *.json file
+2. Download *.json file
+3. Import to MySQL
+4. Export from MongoDB
+---
+Sample *.Json
+----
+```
+
+```
+what need for executing program, run, 
+fill mongo, downaload json, etc
+
--- grails-app/controllers/am/neovision/FileController.groovy
+++ grails-app/controllers/am/neovision/FileController.groovy
@@ -1,22 +1,15 @@
 package am.neovision
 
-import grails.converters.JSON
-import org.codehaus.groovy.grails.web.json.JSONArray
-import org.codehaus.groovy.grails.web.json.JSONObject
-
 class FileController {
 
     def fileService
     def mockService
-
-    def index() { }
 
     def uploadJsonFileToMySQL(){
         def input = request.getFile("jsonfile").inputStream.text
         fileService.saveInMySQL(input)
         render view: '../index'
     }
-
 
     def saveToMongo(){
         mockService.fillTempObject()
