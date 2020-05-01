7b1905b85668a86e398ab7abe06f737b2b11d02f
Hamid <hamidfarmani1@gmail.com>
2020-05-01 15:36:52 +0430

    - delete all data from MongoDB in first-run - Insert mock data in MongoDB in first-run

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import am.neovision.ObjectStorage
+
 class BootStrap {
 
+    def mockService
     def init = { servletContext ->
+        ObjectStorage.collection.remove([ _id:[ $gt:0 ] ])
+        mockService.fillTempObject()
     }
     def destroy = {
     }
